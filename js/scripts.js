14329bc5bfd895aa7f6ececb1d6027ae31d85eae
orishicha <orishicha@gmail.com>
2021-11-05 15:20:33 +0100

    update variables
    introduce Pokemons list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,28 @@
-alert('Hello world!');
+let pokemonList = [
+  {
+    name: 'Charmander',
+    height: 0.6,
+    type: ['fire']
+  },
+  {
+    name: 'Beedrill',
+    height: 1,
+    type: ['bug', 'poison']
+  },
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: ['electric']
+  },
+  {
+    name: 'Jigglypuff',
+    height: 0.5,
+    type: ['fairy', 'normal']
+  },
+  {
+    name: 'Psyduck',
+    height: 0.8,
+    type: ['water']
+  }
+];
 
-let favoriteFood = 'ramen';
-document.write(favoriteFood);
