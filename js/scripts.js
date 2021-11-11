13522ea5b3cacbc9db59a3cdbcdb5d3457c53ee0
orishicha <orishicha@gmail.com>
2021-11-12 00:02:44 +0100

    add IIFE with keys "getAll" and "add"
    wrap pokemonList array in IIFE with keys getAll and add

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,47 @@
-//Pokemon list
-let pokemonList = [
-  {
-    name: 'Charmander',
-    height: 0.6,
-    type: ['fire']
-  },
-  {
-    name: 'Beedrill',
-    height: 1,
-    type: ['bug', 'poison']
-  },
-  {
-    name: 'Pikachu',
-    height: 0.4,
-    type: ['electric']
-  },
-  {
-    name: 'Jigglypuff',
-    height: 0.5,
-    type: ['fairy', 'normal']
-  },
-  {
-    name: 'Psyduck',
-    height: 0.8,
-    type: ['water']
+//IIFE wrap
+let pokemonRepository = (function () {
+  //Pokemon list
+  let pokemonList = [
+    {
+      name: 'Charmander',
+      height: 0.6,
+      type: ['fire']
+    },
+    {
+      name: 'Beedrill',
+      height: 1,
+      type: ['bug', 'poison']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      type: ['electric']
+    },
+    {
+      name: 'Jigglypuff',
+      height: 0.5,
+      type: ['fairy', 'normal']
+    },
+    {
+      name: 'Psyduck',
+      height: 0.8,
+      type: ['water']
+    }
+  ];
+  
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-];
+  
+  function getAll() {
+    return pokemonList;
+  }
+  
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 //Declare function to show Pokemon
 function myPokemonPrintout(pokemon) {
@@ -38,8 +54,13 @@ function myPokemonPrintout(pokemon) {
 }
 
 document.write('<ul>');
+
+//Add a pokemon
+pokemonRepository.add({name: 'Rapidash', height: 1.7, type: ['fire']})
+
 //Call forEach function to show Pokemon list
-pokemonList.forEach(myPokemonPrintout);
+pokemonRepository.getAll().forEach(myPokemonPrintout);
+
 document.write('</ul>');
 
 
